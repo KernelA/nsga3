d7c120d65ab5c36f405eee2a3f4becb6093136da
KernelA <alex9x99@gmail.com>
2017-11-24 21:50:02 +0300

    The None-dominated sorting changed.
    - The class '_FitnessFront' removed. Instead, the dictionary used.
    - The unit test removed. The case, where fitness has only one value.

--- src/tests_ndomsort.py
+++ src/tests_ndomsort.py
@@ -1,8 +1,6 @@
 import unittest
 import ndomsort as nds
 
-def identity(x):
-    return x
 
 class Testndomsort(unittest.TestCase):
 
@@ -10,7 +8,7 @@ class Testndomsort(unittest.TestCase):
     def test_non_domin_sort_many_fronts(self):
         seq = [(i,) * 4 for i in range(20)]
 
-        res = nds.non_domin_sort(seq, identity)
+        res = nds.non_domin_sort(seq)
 
         self.assertEqual(len(res), 20)
 
@@ -21,7 +19,9 @@ class Testndomsort(unittest.TestCase):
     def test_non_domin_sort_one_elem(self):
         seq = [(2,3,4)]
 
-        res = nds.non_domin_sort(seq, identity)
+        res = nds.non_domin_sort(seq)
+
+        self.assertEqual(len(res), 1)
 
         self.assertTupleEqual(seq[0], res[0][0])
 
@@ -29,7 +29,7 @@ class Testndomsort(unittest.TestCase):
 
         seq = ((1,1), (1,1), (1,1))
 
-        res = nds.non_domin_sort(seq, identity)
+        res = nds.non_domin_sort(seq)
 
         self.assertEqual(len(res), 1)
 
@@ -39,26 +39,13 @@ class Testndomsort(unittest.TestCase):
     def test_non_domin_sort_two_front(self):
         seq = ((1,0,1), (0,1,1), (-2, -3, 0))
 
-        res = nds.non_domin_sort(seq, identity)
+        res = nds.non_domin_sort(seq)
 
         self.assertEqual(len(res), 2)
 
         for res_seq in res[1]:
             self.assertIn(res_seq, seq[:2])
 
-    def test_non_domin_sort_one_fitness(self):
-
-        seq =  ( (1,), (1,), (2,), (-4,), (0,) )
-
-        res = nds.non_domin_sort(seq, identity)
-        
-        self.assertEqual(len(res), 4)
-
-        seq_list = sorted(set(seq))
-
-        for front in res:
-            for res_seq in res[front]:
-                self.assertTupleEqual(res_seq, seq_list[front])
 
 if __name__ == "__main__":
     unittest.main()
